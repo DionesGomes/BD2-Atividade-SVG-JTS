e6274274a8583da5610591af6b122cb08c75ae6a
Diones Gomes <diones.zack.dz@gmail.com>
2018-11-29 21:03:58 -0300

    Adicionado a classe responsável por connectar a aplicação com o banco de dados, -ConnectionFactory-

---
+++ Atividade-SVG-JTS/src/main/java/com/br/atividade/svg/jts/model/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package com.br.atividade.svg.jts.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        url = "jdbc:postgresql://localhost:5432/Brasil";
+        user = "postgres";
+        password = "falltopieces";
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
