da4a3b3e52e76221180f305557cea2161b5aa2d7
Diones Gomes <diones.zack.dz@gmail.com>
2018-11-29 21:53:17 -0300

    Foram aplicadas algumas correções nós desvios condicionais presentes na classe -Controller-

--- Atividade-SVG-JTS/src/main/java/com/br/atividade/svg/jts/controller/Controller.java
+++ Atividade-SVG-JTS/src/main/java/com/br/atividade/svg/jts/controller/Controller.java
@@ -1,7 +1,13 @@
 package com.br.atividade.svg.jts.controller;
 
+import com.br.atividade.svg.jts.model.dao.CidadeDaoImpl;
+import com.br.atividade.svg.jts.model.domain.Cidade;
+import com.vividsolutions.jts.io.ParseException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,29 +22,29 @@ public class Controller extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            Command CidadeCommand = new CidadeCommand();
-            CidadeCommand.execute(request, response);
+            Command CidadeCommand = new SvgCommand();
+            CidadeCommand.ToCall(request, response);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CidadeDAOImpl dao = new CidadeDAOImpl();
-        String estadoAtual1 = request.getParameter("estado1");
-        String estadoAtual2 = request.getParameter("estado2");
-        String cidadeNome1 = request.getParameter("cidade1");
-        String cidadeNome2 = request.getParameter("cidade2");
-        request.setAttribute("estado1", estadoAtual1);
-        request.setAttribute("estado2", estadoAtual2);
-        request.setAttribute("cidadeNome1", cidadeNome1);
-        request.setAttribute("cidadeNome2", cidadeNome2);
+        CidadeDaoImpl dao = new CidadeDaoImpl();
+        String estadoAtual1 = request.getParameter("estado_1");
+        String estadoAtual2 = request.getParameter("estado_2");
+        String cidadeNome1 = request.getParameter("cidade_1");
+        String cidadeNome2 = request.getParameter("cidade_2");
+        request.setAttribute("estado_1", estadoAtual1);
+        request.setAttribute("estado_2", estadoAtual2);
+        request.setAttribute("cidadeNome_1", cidadeNome1);
+        request.setAttribute("cidadeNome_2", cidadeNome2);
         Cidade cidade1 = null, cidade2 = null;
-//--------------------------------------------------------------------------------------//		
+
         if (!estadoAtual1.equals("___NENHUM___") && estadoAnterior1 != estadoAtual1) {
             estadoAnterior1 = estadoAtual1;
             try {
-                List<String> cidades1 = dao.buscarNomeCidadesEstado(estadoAtual1);
+                List<String> cidades1 = dao.buscarNomeCidadeUF(estadoAtual1);
                 request.setAttribute("nomeCidades1", cidades1);
             } catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
@@ -48,7 +54,7 @@ public class Controller extends HttpServlet {
         if (!estadoAtual2.equals("___NENHUM___") && estadoAnterior2 != estadoAtual2) {
             estadoAnterior2 = estadoAtual2;
             try {
-                List<String> cidades2 = dao.buscarNomeCidadesEstado(estadoAtual2);
+                List<String> cidades2 = dao.buscarNomeCidadeUF(estadoAtual2);
                 request.setAttribute("nomeCidades2", cidades2);
             } catch (ClassNotFoundException | SQLException e) {
                 e.printStackTrace();
@@ -57,7 +63,7 @@ public class Controller extends HttpServlet {
 
         if (!cidadeNome1.equals("___NENHUM___")) {
             try {
-                cidade1 = dao.buscarCidadeEstado(cidadeNome1, estadoAtual1);
+                cidade1 = dao.buscarCidadeUF(cidadeNome1, estadoAtual1);
                 request.setAttribute("cidade1", cidade1);
             } catch (ClassNotFoundException | SQLException | ParseException e) {
                 e.printStackTrace();
@@ -68,7 +74,7 @@ public class Controller extends HttpServlet {
 
         if (!cidadeNome2.equals("___NENHUM___")) {
             try {
-                cidade2 = dao.buscarCidadeEstado(cidadeNome2, estadoAtual2);
+                cidade2 = dao.buscarCidadeUF(cidadeNome2, estadoAtual2);
                 request.setAttribute("cidade2", cidade2);
             } catch (ClassNotFoundException | SQLException | ParseException e) {
                 e.printStackTrace();
@@ -91,14 +97,14 @@ public class Controller extends HttpServlet {
             request.setAttribute("viewBox", null);
 
         }
-//------------------------------------------------------------------------------------//		
+
         try {
-            List<String> estados = dao.buscarNomeEstados();
+            List<String> estados = dao.buscarNomeUF();
             request.setAttribute("estados", estados);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-//------------------------------------------------------------------------------------//		
+
         request.getRequestDispatcher("View.jsp").forward(request, response);
     }
 
